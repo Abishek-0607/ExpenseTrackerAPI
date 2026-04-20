af98492357c261307b72a00d816c3179c8f53f86
AbishekSundaresan <trainerabi0607@gmail.com>
2026-04-20 17:01:56 +0530

    feat: added swagger for documentation and libraries in requirements

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from routes.expenses import expenses_bp
 from routes.categories import categories_bp
 from flask_cors import CORS
 from config import Config
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
+
+    swagger = Swagger(app)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
