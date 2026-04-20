1f312bb2644ef9d8a1662e892f9487973f0761ef
AbishekSundaresan <trainerabi0607@gmail.com>
2026-04-20 15:45:36 +0530

    feat: hardcoded values into environment

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask
+from routes.health import health_bp
 from database import db
 from routes.expenses import expenses_bp
+from flask_cors import CORS
+from config import Config
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expenses.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
     db.init_app(app)
 
+    app.config.from_object(Config) #loading env variables into app config
+
+    CORS(app, origin=[Config.FRONTEND_URL])
     with app.app_context():
+
         db.create_all()
 
     app.register_blueprint(expenses_bp)
+    app.register_blueprint(health_bp)
 
     return app
-
 
 if __name__ == "__main__":
     app = create_app()
